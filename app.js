87aa60020e70a3f11de836e4e72a189d92b77291
M.M.05 <84689354+Mu-bina-45@users.noreply.github.com>
2021-08-28 12:16:18 -0500

    project2

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const closedFace = document.querySelector('.closed')
-const openFace = document.querySelector('.open')
+let data = [
+    {
+        name: 'Mateoo',
+        age: '30'
+    },
+    {
+        name: 'Sara',
+        age: '32'
+    },
+    {
+        name: 'John',
+        age: '28'
+    },
+    {
+        name: 'Tim',
+        age: '27'
+    },
+    {
+        name: 'Sam',
+        age: '22'
+    }, {
+        name: 'Joey',
+        age: '34'
+    },
+];
 
-//add event listener
-closedFace.addEventListener('click', () => {
-    if (openFace.classList.contains('open')) {
-        openFace.classList.add('active');
-        closedFace.classList.remove('active');
-    }
+const info = document.querySelector('#info');
+
+let details = data.map(function (item) {
+    return (
+        '<div>' + item.name + ' ' + 'is' + ' ' + item.age + ' years old' + '</div >'
+    )
 });
 
-openFace.addEventListener('click', () => {
-    if (closedFace.classList.contains('closed')) {
-        closedFace.classList.add('active');
-        openFace.classList.remove('active');
-    }
-})
+info.innerHTML = details.join('\n');
